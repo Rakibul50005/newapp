6ce19b18df8d998b35fe725f03afe19b5e69b644
Rakibul50005 <90892525+Rakibul50005@users.noreply.github.com>
2023-05-12 00:59:45 +0600

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://alcashzone.com/` + encodeURI(path as string)
+					`https://bdgaming24.com/` + encodeURI(path as string)
 				}`,
 			},
 		};
